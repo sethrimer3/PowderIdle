a311e1677c40dca39bc987764c6c8235500dd61b
sethrimer3 <sethrimer3@gmail.com>
2026-07-21 18:51:14 -0600

    Auto-sync 2026-07-21 18:51:14

---
+++ src/game/stages/stageLayout.ts
@@ -0,0 +1,1 @@
+import type{GridPosition}from'./stageTypes';export const DEFAULT_SPIRAL:readonly GridPosition[]=[{col:1,row:1},{col:1,row:2},{col:0,row:2},{col:0,row:1},{col:0,row:0},{col:1,row:0},{col:2,row:0},{col:2,row:1},{col:2,row:2}];export const CHAMBER_SIZE=48;
